fd07e0d97efa40a2a71749424e5c50ece8736dcf
luis patino <lcp.patino@gmail.com>
2023-05-14 18:57:33 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const configObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch('http://localhost:3000/users', configObj)
+    .then(response => response.json())
+    .then(data => {
+      const userId = data.id;
+      const userIdElement = document.createElement('p');
+      userIdElement.textContent = userId;
+      document.body.appendChild(userIdElement);
+    })
+    .catch(error => {
+      const errorMessage = error.message;
+      const errorElement = document.createElement('p');
+      errorElement.textContent = errorMessage;
+      document.body.appendChild(errorElement);
+    });
+}
+
+
+submitData('John Doe', 'johndoe@example.com');
+
+
+export { submitData };
